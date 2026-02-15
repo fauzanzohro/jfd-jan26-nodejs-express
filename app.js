11b18a7d6d263f86d50186ed8c1b34066f6a148e
Fauzan Zohro <fauzanzohro@gmail.com>
2026-02-15 21:25:43 +0700

    menambahkan halaman profil.beranda,dan detail-pegalaman menggunakan ejs

--- app.js
+++ app.js
@@ -11,9 +11,23 @@ app.get("/", (req, res) => {
 app.get("/profil", (req, res) => {
   res.render("profil");
 });
+app.get("/pengalaman", (req, res) => {
+  let namaLengkap = "Fauzan Zohro";
+  //1. data harus di kirim ke view
+  //2. data harus di panggil di dalam view
+  res.render("detail-pengalaman", {
+    nama: namaLengkap,
+    alamat: "Lbuklinggau",
+    posisi: "programer",
+    perusahaan: "PT Indofood Sedap",
+    gaji: 1100000,
+  });
+});
+
 app.use((req, res) => {
   res.status(404).send("maaf url kamu salah");
 });
+
 app.listen(port, () => {
   console.log(`Aplikasi Berjalan di port http://localhost:${port}`);
 });
